11020bd3b538855541e18998f6c18f8fd60dca04
Paolo Balzarotti <paolo.balzarotti@gmail.com>
2019-05-28 15:57:55 +0200

    Convertiti i dati nel formato correttow

--- dow_jones_index2.py
+++ dow_jones_index2.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 df = pd.read_csv("dow_jones_index/dow_jones_index.data")
 df['date'] = pd.to_datetime(df['date'])
-print(df)
 def converti_valori_monetari(stringa):
     valore = stringa.replace('$','')
     return float(valore)
 colonne_da_convertire = ('open','high','low','close','next_weeks_open','next_weeks_close')
 for colonna in colonne_da_convertire:
     df[colonna] = df[colonna].apply(converti_valori_monetari)
-print(df.dtypes)
+print(df)
+print(df.describe)
