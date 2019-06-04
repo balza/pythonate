c9254872af17e5f4527b16e16a2c355ebe8e6fc1
Paolo Balzarotti <paolo.balzarotti@gmail.com>
2019-06-04 15:14:06 +0200

    Generazione grafici immagini

--- prestiti_e_alberi_decisionali/prestiti.py
+++ prestiti_e_alberi_decisionali/prestiti.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 pd.set_option('display.float_format', lambda x:'{0:.2f}'.format(x))
 df = pd.read_csv("storico_prestiti.csv")
@@ -19,3 +20,29 @@ le_tipologia = LabelEncoder()
 le_tipologia = le_tipologia.fit(df['tipologia'])
 df_encoded.tipologia = le_tipologia.transform(df.tipologia)
 print(df_encoded.head)
+plt.rcParams['figure.figsize'] = (10,10)
+plt.style.use('ggplot')
+plt.hist(df.estinto)
+plt.savefig('prestiti.jpg')
+plt.cla()
+conteggio = df.livello.value_counts()
+conteggio.plot.bar()
+plt.savefig('prestiti1.jpg')
+plt.cla()
+conteggio = df.eta.value_counts()
+conteggio.plot.bar()
+plt.savefig('prestiti2.jpg')
+plt.cla()
+plt.scatter(df.eta,df.saldo,alpha=0.5)
+plt.cla()
+df.plot.scatter(
+    x = 'eta',
+    y = 'saldo',
+    c = 'estinto',
+    logx = True,
+    logy = True,
+    cmap = 'autumn'    
+)
+plt.savefig('prestiti4.jpg')
+plt.cla()
+
